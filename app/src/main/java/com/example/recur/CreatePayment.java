b0a73c049c4f44bcd078125706baba7e4655c9b4
Octavio <octavio.hermida.97@gmail.com>
2019-10-13 00:41:03 -0700

    Added some backend to create a payment

--- app/src/main/java/com/example/recur/CreatePayment.java
+++ app/src/main/java/com/example/recur/CreatePayment.java
@@ -3,7 +3,10 @@ package com.example.recur;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class CreatePayment extends AppCompatActivity {
@@ -12,5 +15,29 @@ public class CreatePayment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_payment);
+    }
+
+    public void setPaymentValues() {
+        try {
+            EditText nameText = findViewById(R.id.nameInput);
+            String name = nameText.getText().toString();
+
+            EditText amountText = findViewById(R.id.amountInput);
+            double amount = Double.parseDouble(amountText.getText().toString());
+
+            EditText intervalText = findViewById(R.id.intervalInput);
+            int interval = Integer.parseInt(intervalText.getText().toString());
+
+            EditText endDateText = findViewById(R.id.endDateInput);
+            Date endDate = new SimpleDateFormat("MM/DD/YYYY").parse(endDateText.getText().toString());
+
+            EditText categoryText = findViewById(R.id.categoryInput);
+            String category = intervalText.getText().toString();
+
+            Payment newPayment = new Payment(name, amount, interval, endDate, category);
+            System.out.println(newPayment.toString());
+        } catch (Exception e) {
+            System.out.println("Failed to create payment");
+        }
     }
 }
--- app/src/main/java/com/example/recur/Payment.java
+++ app/src/main/java/com/example/recur/Payment.java
@@ -5,19 +5,24 @@ import java.util.Date;
 public class Payment {
     String name;
     double amount;
-    int recur;
-    Date startDate;
+    int interval;
     Date endDate;
+    String category;
 
-    public Payment(String nameIn, double amountIn, int repeat, Date start, Date end) {
-        name = nameIn;
-        amount = amountIn;
-        recur = repeat;
-        startDate = start;
-        endDate = end;
+    public Payment(String name, double amount, int interval, Date endDate, String category) {
+        this.name = name;
+        this.amount = amount;
+        this.interval = interval;
+        this.endDate = endDate;
+        this.category = category;
     }
 
     public Payment paymentInfo() {
         return this;
+    }
+
+    public String toString() {
+        return "I'm paying " + this.name + " $" + this.amount + " every " + this.interval +
+                " until " + this.endDate + " for " + this.category;
     }
 }
